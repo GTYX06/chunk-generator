f871852e9a12e4b9d82eb21e9fd0e57b70268bf6
foodisyammy <aaronleekhoo@gmail.com>
2026-07-21 17:42:24 +0800

    Fix loop range and data type for variable C

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ fn main() {
     let A = 7;
     let B = A * 3;
     println!("A times 3 = {}",B);
-    let mut C = -2147483648;
-    while C != 2147483647{
-        println!("Numbers");
+    let mut C = -32768;
+    while C: i16 != 32767 {
+        println!("{}",C);
         C+=1
     }
 }
