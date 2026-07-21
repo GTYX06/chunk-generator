19a3f60a36a96a027152db0bbeed92895e4890a8
foodisyammy <aaronleekhoo@gmail.com>
2026-07-21 17:44:03 +0800

    Fix type declaration and condition in while loop

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ fn main() {
     let A = 7;
     let B = A * 3;
     println!("A times 3 = {}",B);
-    let mut C = -32768;
-    while C: i16 != 32767 {
+    let mut C: i16 = -32768;
+    while C != 32767 {
         println!("{}",C);
         C+=1
     }
