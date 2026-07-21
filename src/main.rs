f568a3d9559316963092a0ca8df243bf869033f3
foodisyammy <aaronleekhoo@gmail.com>
2026-07-21 17:35:01 +0800

    Fix loop condition in main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ fn main() {
     let B = A * 3;
     println!("A times 3 = {}",B);
     let mut C = −2147483648;
-    while C != -3000000{
-        println!("Numbers")
+    while C != 2147483648{
+        println!("Numbers");
+        C+=1
     }
 }
