90cb4927ee32960595ce3ac31f2449b639c3b3f9
foodisyammy <aaronleekhoo@gmail.com>
2026-07-21 17:36:20 +0800

    Correct negative sign for variable C initialization
    Fix the negative sign in the initialization of variable C.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ fn main() {
     let A = 7;
     let B = A * 3;
     println!("A times 3 = {}",B);
-    let mut C = −2147483648;
+    let mut C = -2147483648;
     while C != 2147483648{
         println!("Numbers");
         C+=1
