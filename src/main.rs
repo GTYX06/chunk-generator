9c4a5a34d8038dfbf1fbdd3b198908ad9d6412e6
GTYX06 <ryankhooyuan@gmail.com>
2026-07-22 13:22:23 +0800

    Added input

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
+use std::io;
+
 fn main() {
     let A = 7;
     let B = A * 3;
     println!("A times 3 = {}",B);
-    let mut C: i16 = -32768;
-    while C != 32767 {
+    let mut C: i16 = -256;
+    while C != 255 {
         println!("{}",C);
         C+=1
     }
+    let mut input = String::new();
+    
+    io::stdin()
+        .read_line(&mut input)
+        .expect("failed to read input.");
+
+    println!("You typed: {}",input)
 }
